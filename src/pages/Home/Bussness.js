c9fa7101e5213d04e2239d5eee90055879d825fe
AbidHasan0 <abidhasanrazvi@gmail.com>
2022-05-27 22:26:32 +0600

    add to bussess sumary

--- src/pages/Home/Bussness.js
+++ src/pages/Home/Bussness.js
@@ -26,7 +26,7 @@ const Bussness = () => {
 
             </div>
 
-            <div class="stat">
+            <div class="stat mt-2">
                <div class="stat-figure text-secondary">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current text-purple-600"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path></svg>
                </div>
