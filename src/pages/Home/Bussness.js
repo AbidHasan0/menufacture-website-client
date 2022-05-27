b683258bd63d699e8d49b1c59234c6408f69e6a2
AbidHasan0 <abidhasanrazvi@gmail.com>
2022-05-27 22:14:45 +0600

    implement to portfolio

--- src/pages/Home/Bussness.js
+++ src/pages/Home/Bussness.js
@@ -17,7 +17,7 @@ const Bussness = () => {
 
             </div>
 
-            <div class="stat">
+            <div class="stat ms-2">
                <div class="stat-figure text-secondary">
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-8 h-8 stroke-current text-purple-600"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path></svg>
                </div>
--- src/pages/Home/MyPortfolio.js
+++ src/pages/Home/MyPortfolio.js
@@ -9,8 +9,8 @@ const MyPortfolio = () => {
                <h2 class="card-title">Name: Abid Hasan Rejvi</h2>
                <h2 class="card-title">Email: abidhassanrazvi001@gmail.com</h2>
                <h2 class="card-title">Education: SSC-2014,Hsc-2017, AND Wait to honours last year result(CSE) </h2>
-               <p className='text-bold'>List of Technology and Skill- 2.CSS 3.Bootstrap 4.Tailwind 5.JavaScript 6.React 7.Firebase Authentication 8.React-Router 9.React-Dom 10.JWT 11.Node.js 12.MongoDB 13.Express 14.React-Form</p>
-               <p>Best 3 Project Link:
+               <p className='font-bold'>List of Technology and Skill- 2.CSS 3.Bootstrap 4.Tailwind 5.JavaScript 6.React 7.Firebase Authentication 8.React-Router 9.React-Dom 10.JWT 11.Node.js 12.MongoDB 13.Express 14.React-Form</p>
+               <p className='font-bold'>Best 3 Project Link:
 
                </p>
                <a className='text-primary' href="https://lively-phoenix-af3e87.netlify.app">https://lively-phoenix-af3e87.netlify.app</a>
